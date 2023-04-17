7aea005ddaaf82621acd7e1ef7bb31533c4d2f37
lorenasoarys <lorenasoares78@gmail.com>
2023-04-17 13:16:50 -0300

    first commit

---
+++ somar.js
@@ -0,0 +1,5 @@
+var elem = document.getElementById('btnSoma');
+elem.addEventListener('click', function () {
+    var divop = document.getElementById("Somar");
+    divSomar.innerHTML = "Olá, mundo";
+});
